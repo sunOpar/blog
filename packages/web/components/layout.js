b3c944472952331af4a10e076fe4576ff98bc90a
徐亚光 <xygjsj@139.com>
2019-02-22 14:47:33 +0800

    init web

---
+++ packages/web/components/layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+function layout({ children, title = 'This is the default title' }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {children}
+    </div>
+  );
+}
+
+export default layout;
---
+++ packages/web/pages/index.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout';
+function Home() {
+  return (
+    <Layout>
+      <div>Welcome to next.js!</div>
+    </Layout>
+  );
+}
+
+export default Home;
